282612f18b43e736eb32863d17c267616d2b108f
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2018-07-04 18:15:20 +0530

    navigation added
    navigation added

--- src/components/authorPage.js
+++ src/components/authorPage.js
@@ -1,11 +1,45 @@
-import  React from 'react';
+import  React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const AuthorPage = () => {
-	return(
-		<div>
-			Authors Page
-		</div>	
-	);
+class AuthorPage extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state={
+			authors: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:3000/authors')
+			 .then( (response) => {
+				 this.setState({
+					 authors : response.data
+				 })
+			  });
+	}
+
+	
+
+	render() {
+		let data = "Hello";
+		if(this.state.authors.length === 0) {
+			return false;
+		}	
+		
+		data = this.state.authors.map( (user, i) => {
+			return <li key={i}>{user.name}</li>
+	 	});
+		
+		return(
+			<div>
+				<ul>
+					 {data}
+				</ul>
+			</div>	
+		)
+	}	
 };
 
 export default AuthorPage;
